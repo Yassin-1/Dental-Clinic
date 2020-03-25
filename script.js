d06b483d038244d51a4383f89663813afdc58b51
YassinHtira <YassinHtira@github.com>
2020-03-25 04:10:45 -1100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-
-
-$(document).ready(function(){
+$(document).ready(function () {
     // Show hide popover
-    $(".cl-dropdown").click(function(){
+    $(".cl-dropdown").click(function () {
         $(this).find(".cl-dropmenu-item").slideToggle("fast");
     });
 });
-$(document).on("click", function(event){
+$(document).on("click", function (event) {
     var $trigger = $(".cl-dropdown");
-    if($trigger !== event.target && !$trigger.has(event.target).length){
+    if ($trigger !== event.target && !$trigger.has(event.target).length) {
         $(".cl-dropmenu-item").slideUp("fast");
-    }            
+    }
 });
